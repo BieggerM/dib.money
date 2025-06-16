f3767e34bfb134c91d3e8f5a2d590fb05b9d3dda
BieggerM <BieggerM>
2025-06-16 13:53:37 +0000

    updated styles

--- db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { integer, pgTable, text, serial, timestamp } from 'drizzle-orm/pg-core';
-
-
-export const assessments = pgTable("assessments", {
-    id: serial("id").primaryKey(),
-    productName: text("product_name").notNull(),
-    score: integer("score").notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-  });
